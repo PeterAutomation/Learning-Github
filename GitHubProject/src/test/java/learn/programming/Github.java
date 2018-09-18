a5507a1057af4f2405cdab67c93c0eafcc157634
Micheal <Micheal@Micheal-PC.fios-router.home>
2018-09-18 08:47:48 -0700

    Delete Second class

--- GitHubProject/src/test/java/learn/programming/Github.java
+++ GitHubProject/src/test/java/learn/programming/Github.java
@@ -2,7 +2,8 @@ package learn.programming;
 
 public class Github {
 public static void main(String[] args) {
-	System.out.println("This is Github");
+	
+	 System.out.println("Githu: Fetch, pull, and push");
 	
 }
 }
--- GitHubProject/src/test/java/learn/programming/Second.java
+++
@@ -1,9 +0,0 @@
-package learn.programming;
-
-public class Second {
-   public static void main(String[] args) {
-	 
-	   int a=25;
-	   System.out.println("Print out 25: "+a);
-}
-}
