e6c72c2307c009f33aa26bb7c9d7f53bad572819
Micheal <Micheal@Micheal-PC.fios-router.home>
2018-11-06 12:15:36 -0800

    love me

--- GitHubProject/src/test/java/learn/programming/Github.java
+++ GitHubProject/src/test/java/learn/programming/Github.java
@@ -1,17 +1,9 @@
 package learn.programming;
 
-import java.util.Scanner;
-
 public class Github {
 	public static void main(String[] args) {
 
-		Scanner scan = new Scanner(System.in);
-
-		System.out.println(" Please enter your name : ");
-
-		String name = scan.nextLine();
-
-		System.out.println("My name is " + name);
+	   System.out.println("Love me");
 
 	}
 }
