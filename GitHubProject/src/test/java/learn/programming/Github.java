ef64dfe8ecd1a76332a778c0f2529b958360cc2c
Micheal <Micheal@Micheal-PC.fios-router.home>
2018-11-05 17:12:33 -0800

    Enter your name

--- GitHubProject/src/test/java/learn/programming/Github.java
+++ GitHubProject/src/test/java/learn/programming/Github.java
@@ -1,10 +1,17 @@
 package learn.programming;
 
+import java.util.Scanner;
+
 public class Github {
-public static void main(String[] args) {
-	  
-	 System.out.println("Githu: Fetch, pull, and push"+"He iS KISHOR");
-	 System.out.println("YES");
-	
-}
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println(" Please enter your name : ");
+
+		String name = scan.nextLine();
+
+		System.out.println("My name is " + name);
+
+	}
 }
